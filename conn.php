677e77fb398dd4fb624fd739e435c693555f3e28
Nasif-Abdullah <62460389+Nasif-Abdullah@users.noreply.github.com>
2021-11-15 23:09:06 +0600

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,12 @@
 <?php
 
+
+$postName = $_POST['title'];
+$author = $_POST['author'];
+$post = $_POST['blog'];
+$date = $_POST['date'];
+$category = $_POST['category'];
+
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
@@ -9,4 +16,39 @@ $dbname = "dynamic";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
+//check con
+
+if (!$conn) {
+	die("connection failed: ".mysqli_connect_error());
+}
+else{
+$SELECT = "SELECT postName From blog Where postName = ? Limit 1";
+		$INSERT = "INSERT into blog (postName, post, author, date, category) values(?, ?, ?, ?, ?)";
+
+		//prepare statement
+		$stmt = $conn -> prepare($SELECT);
+		$stmt -> bind_param("s", $postName);
+		$stmt -> execute();
+		$stmt -> bind_result($postName);
+		$stmt -> store_result();
+		$rnum = $stmt -> num_rows;
+
+		if ($rnum == 0){
+			$stmt -> close();
+
+			$stmt = $conn->prepare($INSERT);
+			$stmt -> bind_param("sssss", $postName, $post, $author, $date, $category);
+			$stmt -> execute();
+			header('index.php');
+		}
+		else{
+				echo "More than 1 names found";
+		}
+	}
+
+$stmt -> close();
+$conn -> close();
+
+//closing conn
 ?>
+
