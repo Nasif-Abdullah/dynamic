5447b8ea33e400bc9c9bc19a90a36dff6f7657e5
Nasif-Abdullah <62460389+Nasif-Abdullah@users.noreply.github.com>
2021-11-15 21:57:55 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!--Meta tags
+	-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--Bootstrap CDN-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<!--Font Awesome
+	-->
+	<script src="https://kit.fontawesome.com/e69831aecf.js" crossorigin="anonymous"></script>
+
+	<title></title>
+	<!--Stylesheets
+	-->
+	<link rel="stylesheet" type="text/css" href="hero.css">
+	<link rel="stylesheet" type="text/css" href="list.css">
+	<link rel="stylesheet" type="text/css" href="fgiglink.css">
+	<link rel="stylesheet" type="text/css" href="responsive.css">
+	<link rel="stylesheet" type="text/css" href="certificates.css">
+	<!--font:Poppins-->
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400&display=swap');
+
+		*{
+			font-family: "Poppins";
+			margin: 0;
+			padding: 0;
+		}
+
+		hr{
+			width: 25%;
+			border: 4px solid black;
+			box-shadow: 1px solid grey;
+			border-radius: 25px;
+		}
+	</style>
+</head>
+<body>
+	<main>
+		n
+	</main>
+</body>
